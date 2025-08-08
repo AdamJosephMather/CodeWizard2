878dfa2f09e2b82515117c167a691ba6ade780de
Adam Joseph Mather <adamjosephmather@gmail.com>
2025-08-08 16:10:50 -0600

    Fixed a crashing state in the lsp debugger (no idea what was happening)

--- lspdebug.cpp
+++ lspdebug.cpp
@@ -17,14 +17,18 @@ void LspDebug::position(int x, int y, int w, int h) {
 }
 
 void LspDebug::lspmessage(std::string& from, std::string& message) {
-	Cursor c = Cursor();
-	c.head_line = viewbox->lines.size()-1;
-	c.head_char = viewbox->lines[c.head_line].line_text.length();
-	
-	c.anchor_line = c.head_line;
-	c.anchor_char = c.head_char;
-	
-	std::string newstring = "\n\n["+from+"] --> "+trim(message);
-	icu::UnicodeString log = icu::UnicodeString::fromUTF8(newstring);
-	viewbox->insertTextAtCursor(c, log);
+	new std::thread([=]() {
+		std::lock_guard<std::mutex> lock(App::canMakeChanges);
+		Cursor c = Cursor();
+		
+		c.head_line = viewbox->lines.size()-1;
+		c.head_char = viewbox->lines[c.head_line].line_text.length();
+		
+		c.anchor_line = c.head_line;
+		c.anchor_char = c.head_char;
+		
+		std::string newstring = "\n\n["+from+"] --> "+trim(message);
+		icu::UnicodeString log = icu::UnicodeString::fromUTF8(newstring);
+		viewbox->insertTextAtCursor(c, log);
+	});
 }

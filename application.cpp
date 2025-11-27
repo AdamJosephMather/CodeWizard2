efaf53a0df07ea57150ba2b1b0980222941944e1
Adam Joseph Mather <adamjosephmather@gmail.com>
2025-11-26 21:45:39 -0700

    Built - releasing now.

--- application.cpp
+++ application.cpp
@@ -38,7 +38,7 @@ std::string App::empty = "";
 
 int App::major_version = 2;
 int App::minor_version = 1;
-int App::patch_version = 7;
+int App::patch_version = 8;
 icu::UnicodeString App::vnum = icu::UnicodeString();
 std::string App::vnumstr = "";
 

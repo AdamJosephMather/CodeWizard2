2e700f6dacec5feee67016ef8c4d6b22e2b25559
Adam Joseph Mather <adamjosephmather@gmail.com>
2026-07-26 18:26:31 -0600

    updated version

--- application.cpp
+++ application.cpp
@@ -48,7 +48,7 @@
 
 int App::major_version = 2;
 int App::minor_version = 5;
-int App::patch_version = 13; // 🚀 (we now support emojis)
+int App::patch_version = 14; // 🚀 (we now support emojis)
 
 const std::vector<int> version = {App::major_version, App::minor_version, App::patch_version};
 
